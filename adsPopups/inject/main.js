5d45a2448466cc092fcb9dbd13e72806d4585509
toballz <42551340+toballz@users.noreply.github.com>
2024-08-15 05:44:33 -0400

    a
    a

--- adsPopups/inject/main.js
+++ adsPopups/inject/main.js
@@ -1,8 +1,15 @@
 window.open = function () {
   return null; // Return null to indicate the window wasn't opened
 };
+window._ = function () {
+  return null; // Return null to indicate the window wasn't opened
+};
+
 
 //********** */
+//*********** */
+//************ */
+//************* */
 // Override the default document.write behavior
 document.write = function () {
   console.warn("Blocked document.write call");
@@ -18,14 +25,55 @@ window.addEventListener(
     if (e.target.tagName === "IFRAME") {
       e.stopImmediatePropagation();
       e.preventDefault();
-      console.warn("Blocked iframe click event");
+      console.log("Blocked iframe click event");
     }
   },
   true
 );
+
+
 //********** */
+//*********** */
+//************ */
+//************* */
+//listen for new iframe created
+function handleIframeCreation(node) {
+  if (node.tagName === 'IFRAME') {
+    // Add event listener to block popup behavior
+    node.onload = () => {
+      const iframeWindow = node.contentWindow;
+      if (iframeWindow) {
+        iframeWindow.addEventListener('beforeunload', (event) => {
+          if (event.target !== node) {
+            // Prevent popup
+            event.preventDefault();
+            event.returnValue = '';
+          }
+        });
+      }
+    };
+  }
+}
+
+// Create a MutationObserver to monitor DOM changes
+const observer = new MutationObserver((mutations) => {
+  mutations.forEach((mutation) => {
+    mutation.addedNodes.forEach(handleIframeCreation);
+  });
+});
+
+// Start observing the document for changes
+observer.observe(document.documentElement, {
+  childList: true,
+  subtree: true
+});
 
 
+
+//********** */
+//*********** */
+//************ */
+//************* */
 //debugger
 Object.defineProperty(window, "debugger", {
   configurable: true,
