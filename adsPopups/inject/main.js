166ac26bfe5911fc7301d1c8215a55f04484dd23
toballz <42551340+toballz@users.noreply.github.com>
2024-08-18 21:48:51 -0400

    bx
    cbx

--- adsPopups/inject/main.js
+++ adsPopups/inject/main.js
@@ -1,3 +1,51 @@
+function onDocumentLoad() {
+  //********** */
+  //*********** */A
+  //************ */
+  //************* */
+  //listen for new iframe created
+  function handleIframeCreation(node) {
+    if (node.tagName === "IFRAME") {
+      node.onload = function () {
+        try {
+          const iframeWindow = node.contentWindow;
+          iframeWindow.open = function () {
+            return null;
+          };
+        } catch (e) {
+          console.error("Error accessing iframe content:", e);
+        }
+      };
+      return null;
+    }
+  }
+
+  // Create a MutationObserver to monitor DOM changes
+  const observer = new MutationObserver((mutations) => {
+    mutations.forEach((mutation) => {
+      mutation.addedNodes.forEach(handleIframeCreation);
+    });
+  });
+
+  // Start observing the document for changes
+  observer.observe(document.documentElement, {
+    childList: true,
+    subtree: true,
+  });
+  
+  //********** */
+  //*********** */
+  //************ */
+  //************* */
+  //href
+  document.querySelectorAll("a[target]").forEach(function (link) {
+    link.addEventListener("click", function (event) {
+      event.preventDefault();
+      return null;
+      // You can add any other logic you want to execute here
+    });
+  });
+}
 window.open = function () {
   return null; // Return null to indicate the window wasn't opened
 };
@@ -10,7 +58,7 @@ window._ = function () {
 //************ */
 //************* */
 // Override the default document.write behavior
-document.write = function () { 
+document.write = function () {
   return null;
 };
 document._ = function () {
@@ -22,34 +70,15 @@ document._ = function () {
 //*********** */
 //************ */
 //************* */
-//listen for new iframe created
-function handleIframeCreation(node) {
-  if (node.tagName === "IFRAME") { 
-    return null;
-  }
-}
-
-// Create a MutationObserver to monitor DOM changes
-const observer = new MutationObserver((mutations) => {
-  mutations.forEach((mutation) => {
-    mutation.addedNodes.forEach(handleIframeCreation);
-  });
-});
-
-// Start observing the document for changes
-observer.observe(document.documentElement, {
-  childList: true,
-  subtree: true,
-});
-
-//********** */
-//*********** */
-//************ */
-//************* */
 //debugger
 Object.defineProperty(window, "debugger", {
   configurable: true,
   enumerable: false,
   writable: true,
   value: function () {},
+});
+
+//
+document.addEventListener("DOMContentLoaded", function () {
+  onDocumentLoad();
 });
