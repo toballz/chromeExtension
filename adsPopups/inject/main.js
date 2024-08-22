c86b3e9f90212da3187ae2100e57c666ed72abc5
toballz <42551340+toballz@users.noreply.github.com>
2024-08-22 16:11:14 -0400

    b
    b

--- adsPopups/inject/main.js
+++ adsPopups/inject/main.js
@@ -6,6 +6,8 @@ function stopTR() {
 function onDocumentLoad(w = window) {
   const { HTMLAnchorElement, HTMLFormElement } = w;
   const wd = w.document;
+  //
+  //
   // Disable certain actions
   w.open = stopTR;
   wd.write = stopTR;
@@ -16,8 +18,17 @@ function onDocumentLoad(w = window) {
   HTMLFormElement.prototype.submit = stopTR;
   HTMLFormElement.prototype.dispatchEvent = stopTR;
 
+  //
+  //
   // Prevent default action on anchor tags with target attribute
   wd.addEventListener("DOMContentLoaded", function () {
+    wd.querySelectorAll("base[target]").forEach(function (link) {
+      link.setAttribute("target", "#");
+      link.remove();
+    });
+    //
+    //
+    //
     wd.querySelectorAll("a[target]").forEach(function (link) {
       link.addEventListener("click", function (event) {
         event.preventDefault();
@@ -26,13 +37,20 @@ function onDocumentLoad(w = window) {
     });
   });
 
+  //
+  //
   // Listen for new iframes created
   const observer = new MutationObserver((mutations) => {
     mutations.forEach((mutation) => {
       mutation.addedNodes.forEach((node) => {
-        if (node.tagName === "IFRAME") {
+        const iframeWindow = node.contentWindow;
+
+        if (node.tagName === "IFRAME" || node.tagName === "FRAME") {
+          // const src = node.src.toLowerCase();
+          // if (src.startsWith("javascript:") || src.startsWith("data:")) {
+          //   node.remove();
+          // }
           try {
-            const iframeWindow = node.contentWindow;
             onDocumentLoad(iframeWindow); // Apply the function to the iframe's contentWindow
           } catch (e) {
             //console.log("Error accessing iframe content:", e);
@@ -44,7 +62,18 @@ function onDocumentLoad(w = window) {
     childList: true,
     subtree: true,
   });
-}
 
+  //
+  //
+  // Override iframe src setter to prevent loading
+  Object.defineProperty(HTMLIFrameElement.prototype, "src", {
+    set: function () {
+      //console.warn("Blocked attempt to set iframe src.");
+    },
+    configurable: true,
+  });
+}
+//
+//
 // Apply the function to the current document
 onDocumentLoad();
